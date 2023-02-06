beb769946b6db88a4cfa5a52e8b7dfd321c4943f
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-02-06 12:34:08 +0100

    Send 2D embeddings through API endpoint

--- backend/src/crud/newsgroups.py
+++ backend/src/crud/newsgroups.py
@@ -1,4 +1,6 @@
+from datasets import load_dataset
+
 async def get_all_embeddings():
-    
-    
-    return ""
+    dataset = load_dataset("fscheffczyk/2D_20newsgroup_embeddings", split="train")
+    json_data = dataset.to_dict()
+    return json_data
