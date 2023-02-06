f9ec3a48065b17765a8e15f8ce826745aa208d68
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-02-06 22:52:13 +0100

    Render example pts canvas

--- frontend/src/lib/Plot.svelte
+++ frontend/src/lib/Plot.svelte
@@ -1,1 +1,27 @@
-jkef bjk
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { CanvasSpace, Rectangle } from "pts";
+
+    let container, space, form;
+
+    onMount(() => {
+      space = new CanvasSpace(container).setup({bgcolor: "#52f"});
+      form = space.getForm();
+          
+      space.add( time => {
+              form.fillOnly("#f06").point(space.pointer, 200, "circle");
+              form.strokeOnly("#fff", 5).lines( 
+                  Rectangle.corners(space.innerBound).map(r => [r, space.pointer])
+              );
+              form.fillOnly("#0c9").point(space.pointer, 100, "circle");
+          });
+          
+      space.bindMouse().bindTouch().play();
+    });
+</script>
+
+<canvas id="pts" bind:this={container} /> 
+  
+<style>
+    #pts { width: 100vw; height: 100vh; }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+    // ----- Components -----
+    import Plot from '$lib/Plot.svelte';
+
+    // ----- Data -----
     import type { PageData } from './$types';
 
     export let data: PageData;
 </script>
 
-<p>{ data.embeddings }</p>
+<Plot />
