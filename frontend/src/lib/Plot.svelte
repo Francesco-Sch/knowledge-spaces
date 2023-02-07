629f0fe6992df3524f2d44984c2439077832d041
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-02-07 11:36:27 +0100

    Remove p5.js

--- frontend/src/lib/Plot.svelte
+++ frontend/src/lib/Plot.svelte
@@ -2,36 +2,6 @@
     import { onMount } from "svelte";
 
     export let data;
-
-    let sketchContainer: any;
-
-    const s = ( sketch ) => {
-
-        sketch.setup = () => {
-            sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-
-            sketch.background(139);
-            sketch.fill(255);
-
-            for(const circle in data) {
-                let mappedX: number = sketch.map(circle[0], -0.5, 0.5, 0, sketch.windowWidth);
-                let mappedY: number = sketch.map(circle[1], -0.5, 0.5, 0, sketch.windowHeight);
-                sketch.circle(mappedX/10, mappedY/10, 10)
-            }
-        }
-        sketch.draw = () => {
-            
-        };
-
-        //  sketch.windowResized = () => {
-        //     sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
-        //  }
-    };
-
-    onMount(() => {
-        let p5sketch = new p5(s, sketchContainer);
-        p5sketch
-    });
 </script>
 
 <div id="p5" bind:this={sketchContainer} /> 
