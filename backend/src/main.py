e58fb684e0351474324e7390a07884ed03955939
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-02-12 20:17:23 +0100

    Dockerize SvelteKit with docker-compose

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # --- Middleware --- 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
