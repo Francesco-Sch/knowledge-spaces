58d43b0d0343ad3b5fed4828d368ee9a6c94b023
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-06-07 13:18:34 +0200

    Minimal refactor of main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,18 +6,22 @@ import loader
 
 app = FastAPI()
 
-# --- Middleware --- 
+# --- CORS --- 
+origins = ["http://localhost:5173"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# --- ROUTER ---
 app.include_router(embeddings.router)
 app.include_router(search.router)
 
+# --- STARTUP ---
 @app.get("/")
 def home():
     return "This is the API for the knowledge spaces project by Francesco Scheffczyk."
