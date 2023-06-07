2ee9b50ff54b46a7f2f25c9668653000fbe5c01e
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-06-07 15:25:17 +0200

    Add CSS

--- frontend/src/lib/Plot.svelte
+++ frontend/src/lib/Plot.svelte
@@ -3,38 +3,31 @@
     import { CanvasSpace, Pt } from 'pts';
 
     let canvas: Element, windowWidth: number, windowHeight: number;
-    let mapped = [];
-    export let data;
+    export let data: Array<Array<number>>;
 
     function map_range(value, low1, high1, low2, high2) {
         return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
     }
 
-    const calculatedMapped = () => {
-        data.forEach(e => {
-            let tmp = [];
-
-            tmp[0] = map_range(e[0], -0.1, 0.5, 0, windowWidth)
-            tmp[1] = map_range(e[1], -0.1, 0.5, 0, windowHeight)
-
-            mapped.push(tmp)
-        })
-    }
+    const calculatedMapped = (): Array<Array<number>> => {
+    return data.map(([x, y]) => [
+            map_range(x, -0.1, 0.5, 0, windowWidth),
+            map_range(y, -0.1, 0.5, 0, windowHeight)
+        ]);
+    };
     
 
     onMount(() => {
-        let pts
+        let pts: Array<Pt> = [];
 
-        calculatedMapped();
-
-        const space = new CanvasSpace(canvas).setup({ bgcolor: "#f6f6f6", resize: true, retina: true });
+        const space = new CanvasSpace(canvas).setup({ bgcolor: '#EBEAEB', resize: true, retina: true });
         const form = space.getForm();
 
         // zoom
         // const grid = new Grid(bound.width, bound.height, 20, 20, form.center(), 0.5, "#ddd");
         space.add( {
             start: (bound, space) => { 
-                pts = mapped.map( (c) => new Pt( c ) );
+                pts = calculatedMapped().map( (c) => new Pt( c ) );
 
             },
             animate: (time, ftime, space) => { 
