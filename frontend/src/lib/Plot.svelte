6ae3851e076124377252e7e3fc1b0155bbaea435
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-06-07 22:06:27 +0200

    Create function to render a cross

--- frontend/src/lib/Plot.svelte
+++ frontend/src/lib/Plot.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
+	import type { Context } from 'konva/lib/Context';
+	import type { ShapeConfig } from 'konva/lib/Shape';
 	import { onMount } from 'svelte';
-	import { Stage, Layer, Rect } from 'svelte-konva';
+	import { Stage, Layer, Rect, Shape } from 'svelte-konva';
 
-	let canvas: Element, windowWidth: number, windowHeight: number;
+	let windowWidth: number, windowHeight: number;
 	export let data: Array<Array<number>>;
+
+	// Function to draw a cross
+	function cross(context: Context, shape: Shape<ShapeConfig>): void {
+		let width = shape.getAttr('width');
+		let height = shape.getAttr('height');
+
+		// Begin drawing the cross
+		context.beginPath();
+
+		// Move to the top-left corner of the cross
+		context.moveTo(-width, -height);
+
+		// Draw the horizontal line
+		context.lineTo(width, height);
+
+		// Move to the center of the cross
+		context.moveTo(width, -height);
+
+		// Draw the vertical line
+		context.lineTo(-width, height);
+
+		// Close the path
+		context.closePath();
+
+		// Stroke the path to render the cross
+		context.stroke();
+
+		context.fillStrokeShape(shape);
+	}
 
 	onMount(() => {});
 </script>
@@ -12,7 +43,17 @@
 
 <Stage config={{ width: windowWidth, height: windowHeight }}>
 	<Layer>
-		<Rect config={{ x: 100, y: 100, width: 400, height: 200, fill: 'blue' }} />
+		<Shape
+			config={{
+				sceneFunc: cross,
+				x: 100,
+				y: 100,
+				width: 5,
+				height: 5,
+				stroke: 'black',
+				strokeWidth: 2
+			}}
+		/>
 	</Layer>
 </Stage>
 
