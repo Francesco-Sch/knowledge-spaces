513f63e5888407f2d5a178d7e5da5fefc653ccb0
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-06-26 19:30:19 +0200

    Style NearestNeighbors counter

--- frontend/src/lib/settings/NearestNeighbors.svelte
+++ frontend/src/lib/settings/NearestNeighbors.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 	let neighbors = 5;
+
+	// Handle mouse wheel scroll
+	const handleScroll = (event: { preventDefault: () => void; deltaY: number }) => {
+		event.preventDefault();
+		if (event.deltaY > 0) {
+			neighbors--;
+		} else {
+			neighbors++;
+		}
+	};
+
+	// Keep neighbors between 1 and 20
+	$: if (neighbors > 20) {
+		neighbors = 20;
+	}
+	$: if (neighbors < 1) {
+		neighbors = 1;
+	}
 </script>
 
 <div class="nearest-neighbors">
-	<div class="counter">
+	<div class="counter" on:wheel={handleScroll}>
 		<p class="counter_number editorial-new-400">{neighbors}</p>
 		<div class="counter_buttons">
 			<button
@@ -39,6 +57,11 @@
 		flex-direction: row;
 		font-size: 6rem;
 		line-height: 85%;
+
+		transition: all 0.2s ease-in-out;
+	}
+	.counter:hover {
+		text-shadow: var(--hover-text-shadow_black);
 	}
 	.counter .counter_number {
 		margin: 0.5rem 0 0;
@@ -56,6 +79,11 @@
 		text-align: right;
 		background: none;
 		border: none;
+
+		transition: all 0.2s ease-in-out;
+	}
+	.counter:hover .counter_buttons button {
+		text-shadow: var(--hover-text-shadow_black);
 	}
 
 	label {
