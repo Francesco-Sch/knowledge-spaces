07ff4465708951bc59e0d230010ae2dec1556987
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-07-01 12:23:31 +0200

    Create store for search results

--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -3,3 +3,7 @@ import { writable } from 'svelte/store';
 // --- Settings components ---
 export const selectedDataset = writable('redpajama');
 export const neighbors = writable(5);
+
+// --- Searches ---
+export const searchResults = writable(JSON.parse(localStorage.getItem('searchResults') || 'null'));
+searchResults.subscribe((value) => (localStorage.searchResults = JSON.stringify(value)));
