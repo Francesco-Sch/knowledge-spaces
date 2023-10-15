b5f86a711660146a07545dc0f442e24cfe30b203
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2023-10-15 14:18:46 +0200

    Add origins settings to backend

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,7 +1,12 @@
 import type { PageLoad } from './$types';
 
+const BASE_URL = import.meta.env.PROD
+	? 'https://api.knowledge-spaces.com'
+	: 'http://localhost:7100';
+
 export const load = (async ({ fetch }) => {
-	const res = await fetch('http://localhost:7100/embeddings/20newsgroups');
+	const url = `${BASE_URL}/embeddings/20newsgroups`;
+	const res = await fetch(url);
 	const embeddings: object = await res.json();
 
 	return { embeddings: embeddings };
