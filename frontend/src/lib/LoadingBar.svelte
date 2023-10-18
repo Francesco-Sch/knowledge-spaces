f2326137828e8bccaa1935bed7b28d589af49a61
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2023-10-18 15:52:00 +0200

    Add loading spinner to loading bar

--- frontend/src/lib/LoadingBar.svelte
+++ frontend/src/lib/LoadingBar.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
+	import Spinner from './Spinner.svelte';
 	export let loading: boolean = false;
 </script>
 
 <div class="loading-bar">
 	<p class="times-400 loading-bar__message">AI is searching in its knowledge space...</p>
+	<Spinner />
 </div>
 
 <style>
 	.loading-bar {
+		display: flex;
+		flex-direction: row;
 		width: 100%;
 	}
 	.loading-bar__message {
+		flex-grow: 1;
 		margin: 0;
 		padding: 0;
 		font-size: 1.75rem;
