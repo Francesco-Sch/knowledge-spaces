ede035a8d206013e627a7ebb24998a58fdcb8f0d
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-03-04 18:33:28 +0100

    --wip-- [skip ci]

--- frontend/src/data/config.ts
+++ frontend/src/data/config.ts
@@ -1,4 +1,6 @@
-const BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL : 'http://localhost:7100';
+import { env } from '$env/dynamic/public';
+
+const BASE_URL = import.meta.env.PROD ? env.PUBLIC_BACKEND_URL : 'http://localhost:7100';
 console.log('BASE_URL:', BASE_URL);
 
 export { BASE_URL };
