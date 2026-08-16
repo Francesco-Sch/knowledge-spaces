462683b8a146b5a6cd3c5608ae3410e5de4b3f76
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2026-08-16 22:58:43 +0200

    fix(frontend): Preserve explicit point IDs

--- frontend/src/lib/plot/LineToCross.svelte
+++ frontend/src/lib/plot/LineToCross.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
 	import { Line } from 'svelte-konva';
+	import type { Point } from './types';
 
 	export let searchPoint: Array<number>;
-	export let cross: any;
+	export let cross: Point;
 	export let color: string;
 
-	function computeBow(searchPoint, cross) {
+	function computeBow(searchPoint: Array<number>, cross: Point) {
 		// Calculate the slope of the line
-		const slope = (cross[1] - searchPoint[1]) / (cross[0] - searchPoint[0]);
+		const slope = (cross.y - searchPoint[1]) / (cross.x - searchPoint[0]);
 
 		// Calculate the length of the line
 		const lineLength = Math.sqrt(
-			Math.pow(cross[0] - searchPoint[0], 2) + Math.pow(cross[1] - searchPoint[1], 2)
+			Math.pow(cross.x - searchPoint[0], 2) + Math.pow(cross.y - searchPoint[1], 2)
 		);
 
 		// Calculate the bow magnitude based on line length (adjust the multiplier as needed)
 		const bowMagnitude = lineLength * 0.2;
 
 		// Calculate the midpoint and adjust y for the bow effect
-		const midX = (searchPoint[0] + cross[0]) / 2;
-		let midY = (searchPoint[1] + cross[1]) / 2;
+		const midX = (searchPoint[0] + cross.x) / 2;
+		let midY = (searchPoint[1] + cross.y) / 2;
 
 		// Adjust the bow based on slope and magnitude
 		midY += slope > 0 ? -bowMagnitude : bowMagnitude;
@@ -33,14 +34,7 @@
 <Line
 	config={{
 		// @ts-ignore
-		points: [
-			searchPoint[0],
-			searchPoint[1],
-			bowedPoint.midX,
-			bowedPoint.midY,
-			cross[0],
-			cross[1]
-		],
+		points: [searchPoint[0], searchPoint[1], bowedPoint.midX, bowedPoint.midY, cross.x, cross.y],
 		stroke: color,
 		strokeWidth: 2,
 		dash: [5, 5],
